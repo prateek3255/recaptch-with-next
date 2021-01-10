14f229c3a9567938c2be1181517c48b29fc101cc
Prateek Surana <prateeksurana3255@gmail.com>
2021-01-10 14:42:17 +0530

    :sparkles: Added and integrated react-google-recaptcha

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Head from "next/head";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Home() {
   const [email, setEmail] = React.useState("");
+  const recaptchaRef = React.useRef(null);
 
   const handleChange = ({ target: { value } }) => {
     setEmail(value);
@@ -10,8 +12,23 @@ export default function Home() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Execute the reCAPTCHA when the form is submitted
+    recaptchaRef.current.execute();
+  };
+
+  const onReCAPTCHAChange = (captchaCode) => {
+    // If the reCAPTCHA code is null or undefined indicating that
+    // the reCAPTCHA was expired then return early
+    if (!captchaCode) {
+      return;
+    }
+    // Else reCAPTCHA was executed successfully so proceed with the
+    // alert
     alert(`Hey, ${email}`);
-  }
+    // Reset the reCAPTCHA so that it can be executed again if user
+    // submits another email.
+    recaptchaRef.current.reset();
+  };
 
   return (
     <div className="container">
@@ -23,7 +40,19 @@ export default function Home() {
         <h2 className="header">Hello reCAPTCHA</h2>
         <div>
           <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} required type="email" name="email" placeholder="Email" />
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              size="invisible"
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+              onChange={onReCAPTCHAChange}
+            />
+            <input
+              onChange={handleChange}
+              required
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
             <button type="submit">Register</button>
           </form>
         </div>
